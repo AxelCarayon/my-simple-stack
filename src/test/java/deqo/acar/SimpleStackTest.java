009e3fe775238bed7fd9e10ce3410aa8efa0e6e7
AxelCarayon <axel.carayon@gmail.com>
2020-10-19 14:18:51 +0200

    #4 Ajout de la documentation dans les tests

--- src/test/java/deqo/acar/SimpleStackTest.java
+++ src/test/java/deqo/acar/SimpleStackTest.java
@@ -10,6 +10,10 @@ public class SimpleStackTest {
 
     Item i;
     SimpleStack s;
+
+    /**
+     * Crée un item et le stack qui sera utilisé.
+     */
     @Before
     public void setUp() {
         i = new Item();
@@ -20,6 +24,9 @@ public class SimpleStackTest {
     public void tearDown() {
     }
 
+    /**
+     * Teste si le stack est vide à son initialisation et si il n'est pas vide quand on lui rajoute un élément.
+     */
     @Test
     public void isEmpty() {
         try{
@@ -31,6 +38,9 @@ public class SimpleStackTest {
         }
     }
 
+    /**
+     * Teste que la taille est bien de 0 et qu'elle incrémente bien quand on rajoute un élément dedans.
+     */
     @Test
     public void getSize() {
         try{
@@ -42,6 +52,9 @@ public class SimpleStackTest {
         }
     }
 
+    /**
+     * Teste que un élément as bien été ajouté après un push.
+     */
     @Test
     public void push() {
         try{
@@ -53,6 +66,9 @@ public class SimpleStackTest {
         }
     }
 
+    /**
+     * Vérifie que l'item ajouté en tête de liste est bien égal à celui qu'on as mis dans la liste.
+     */
     @Test
     public void peek() {
         try{
@@ -63,6 +79,9 @@ public class SimpleStackTest {
         }
     }
 
+    /**
+     * Teste que la liste est bien vide quand on as enlevé un élément dans la liste.
+     */
     @Test
     public void pop() {
         try{
