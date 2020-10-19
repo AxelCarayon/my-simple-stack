78773a3590d4e88772100bef34cf5bf68f65f58d
AxelCarayon <axel.carayon@gmail.com>
2020-10-19 14:15:03 +0200

    #3 ajout de la documentation dans Item

--- src/main/java/deqo/acar/Item.java
+++ src/main/java/deqo/acar/Item.java
@@ -1,6 +1,9 @@
 package deqo.acar;
 
 public class Item extends Object{
+    /**
+     * Item est identique à un objet, on appelle le super constructeur pour lui donner les même méthodes
+     */
     public Item(){
         super();
     }
